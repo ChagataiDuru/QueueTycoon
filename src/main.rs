8ef14933b0f1e315abcdb1c9abdf500dfeb236fa
Çağatay <durucagatay01@gmail.com>
2024-03-06 09:53:18 +0300

    Basic Windows init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-use bevy::prelude::*;
+mod prelude {
+    pub use bevy::prelude::*;
+    pub use bevy_hanabi::prelude::*;
+    //pub use leafwing_input_manager::prelude::*;
+    //pub use rand::{thread_rng, Rng};
+}
+
+use bevy::window::WindowResolution;
+use crate::prelude::*;
+
+pub const WINDOW_WIDTH: f32 = 1280.0;
+pub const WINDOW_HEIGHT: f32 = 800.0;
 
 fn main() {
-    App::new().run();
+    App::new()
+    .add_plugins(DefaultPlugins.set(
+        WindowPlugin {
+            primary_window: Some(Window {
+                title: "QueueTycoon".to_string(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                ..default()
+            }),
+            ..default()
+        }
+    ))
+    .add_plugins(HanabiPlugin)
+    .add_systems(Startup, setup_camera)
+    .run();
+}
+
+pub fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
